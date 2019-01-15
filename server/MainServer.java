8e75293659f00b6f94c8b484f7182951f7f96676
Brandon Lucier <lucierbrandon@gmail.com>
2019-01-15 12:33:27 -0500

    Some work on part 2

--- server/MainServer.java
+++ server/MainServer.java
@@ -1,67 +1,63 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Date;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.nio.file.*;
+import java.nio.charset.*;
 
-public class MainServer { 
-	// port to listen connection
-	static final int PORT = 8081;
+public class MainServer {
+    static final int PORT = 8081;
+    static final String STATIC_DIR = System.getProperty("user.dir") + "/static";
 
-	// Client Connection via Socket Class
-	private Socket connect;
+    public static String loadStaticFile(String path) throws IOException {
+        File file = new File(STATIC_DIR + path);
+        if(!file.exists()) {
+            return "";
+        }
 
-	public MainServer(Socket c) {
-		connect = c;
-	}
+        byte[] encoded = Files.readAllBytes(Paths.get(STATIC_DIR + path));
+        return new String(encoded, StandardCharsets.UTF_8);
+    }
 
-	public static void main(String[] args) {
-		try {
-			ServerSocket serverConnect = new ServerSocket(PORT);
+    public static void main(String[] args) {
+        try {
+            ServerSocket listener = new ServerSocket(PORT);
 			System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
 
-			// we listen until user halts server execution
-			while (true) {
-				MainServer myServer = new MainServer(serverConnect.accept());
-				myServer.run();
-			}
-		} catch (IOException e) {
-			System.err.println("Server Connection error : " + e.getMessage());
-		}
-	}
+            while(true) {
+                Socket socket = listener.accept();
 
-	public void run()
-	{
-		try {
-			BufferedReader in = null; PrintWriter out = null; BufferedOutputStream dataOut = null;
-			in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-			out = new PrintWriter(connect.getOutputStream());
-			dataOut = new BufferedOutputStream(connect.getOutputStream());
+                if(socket == null) {
+                    break;
+                }
 
-			// get first line of the request from the client
-			String input = in.readLine();
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String header = in.readLine();
+                String[] split = header.split(" ");
 
-			String response = "<html>Hello from our server!</html>";
+                // The path / is a special case as it will also look for index.html.
+                String path = split[1];
+                if(path.equals("/")) {
+                    path = "/index.html";
+                }
 
-			System.out.println("Got this: " + input + "\n");
-			out.println("HTTP/1.1 200 OK");
-			out.println("Server: Cool Server 1.0");
-			out.println("Date: " + new Date());
-			out.println("Content-type: text/html");
-			out.println("Content-length: " + response.length());
-			out.println(); // blank line between headers and content, very important !
-			out.println(response);
-			out.flush(); // flush character output stream buffer
-		} catch (IOException e) {
-			System.err.println("Error running the server: " + e.getMessage());
-		}
-	}
+                String source = loadStaticFile(path);
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                out.write("HTTP/1.1 200 OK\r\n");
+                out.write("Content-Type: text/html\r\n");
+                out.write("Content-Length: " + source.length() + "\r\n");
+                out.write("\r\n");
+                out.write(source);
+                out.flush();
+
+                out.close();
+                in.close();
+                socket.close();
+            }
+
+            listener.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
